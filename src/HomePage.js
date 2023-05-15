dff24e2c9664084e00a62d22f3c2dad1281616e9
Priyanka1377 <priyakaruna2002@gmail.com>
2023-05-15 16:01:14 +0530

    samp_priya

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,9 +9,10 @@ function HomePage({ toggleLoginPage }) {
     </div>
 
     <div className="home-container">
-      <h2>Welcome to Amrita Vishwa Vidyapeetham Canteen System</h2>
+      <h2>Welcome to Amrita Vishwa Vidyapeetham Canteen System ello</h2>
       <div className="container-inner">
         <div className="quote">
+          
         Children, we cannot control our mind without controlling our desire for taste.
         The health aspect, not the taste, should be the prime criteria in choosing the food.
         We cannot relish the blossoming of the heart without foregoing the taste of the tongue.
