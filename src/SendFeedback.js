747258fea0cbb56a5dd45d49c245eb8fc39744e7
sri6703 <cb.en.u4cse20362@cb.students.amrita.edu>
2023-06-14 03:30:59 +0530

    feedback

--- src/SendFeedback.js
+++ src/SendFeedback.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import Loading from "./loading.js";
 
-import "./SendFeedback.css";
+import './SendFeedback.css';
 
 const SendFeedback = ({ userid }) => {
   const [feedbackType, setFeedbackType] = useState("");
@@ -31,37 +31,43 @@ const SendFeedback = ({ userid }) => {
 
   const handleSubmit = () => {
     if (!feedbackType || !feedbackSubtype || feedbackText.length < 50) {
-      setErrorMessage(
-        "Please fill in all required fields and provide at least 50 characters of feedback."
-      );
-      return;
-    }
-
+        setErrorMessage("Please fill in all required fields and provide at least 50 characters of feedback.");
+        return;
+      }
+  
     const feedbackData = {
-      regno: anonymous ? null : userid,
-      canteen: feedbackType === "food" ? feedbackSubtype : null,
-      food: feedbackType === "food" ? feedbackSubtype : null,
-      feedback: feedbackText,
+      userid: anonymous ? null : userid,
+      feedbackType,
+      feedbackSubtype,
+      feedbackText,
     };
 
+    // Perform your Axios POST request here
     setIsLoading(true);
-    axios
-      .post("/feedback", feedbackData)
-      .then((response) => {
-        console.log(response.data);
-        // Reset form fields
-        setFeedbackType("");
-        setFeedbackSubtype("");
-        setFeedbackText("");
-        setAnonymous(false);
-        setErrorMessage("");
+    axios.post("/api/feedback", feedbackData)
+    .then((response) => {
+     console.log(response.data);
+    // Reset form fields
+    setFeedbackType("");
+    setFeedbackSubtype("");
+    setFeedbackText("");
+    setAnonymous(false);
+    setErrorMessage("");
 
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        setIsLoading(false);
-      });
+    setIsLoading(false);
+    })
+    .catch((error) => {
+    console.error(error);
+    });
+
+    // For the sake of this example, simply logging the feedback data
+    console.log(feedbackData);
+
+    // Reset form fields
+    setFeedbackType("");
+    setFeedbackSubtype("");
+    setFeedbackText("");
+    setAnonymous(false);
   };
 
   if (isLoading) {
@@ -73,69 +79,69 @@ const SendFeedback = ({ userid }) => {
       <h1>Feedback</h1>
       <div className="send-feedback-form">
         <div>
-          <label>
-            <input
-              type="checkbox"
-              checked={anonymous}
-              onChange={handleAnonymousChange}
-            />
-            Send Anonymously
-          </label>
-        </div>
+        <label>
+          <input
+            type="checkbox"
+            checked={anonymous}
+            onChange={handleAnonymousChange}
+          />
+          Send Anonymously
+        </label>
+      </div>
+      <div>
+        <label>
+          Feedback Type:
+          <select value={feedbackType} onChange={handleFeedbackTypeChange}>
+            <option value="">Select a type</option>
+            <option value="app">App</option>
+            <option value="food">Food</option>
+          </select>
+        </label>
+      </div>
+      {feedbackType === "food" && (
         <div>
           <label>
-            Feedback Type:
-            <select value={feedbackType} onChange={handleFeedbackTypeChange}>
-              <option value="">Select a type</option>
-              <option value="app">App</option>
-              <option value="food">Food</option>
+            Feedback Subtype:
+            <select
+              value={feedbackSubtype}
+              onChange={handleFeedbackSubtypeChange}
+            >
+              <option value="">Select a subtype</option>
+              <option value="canteen1">Canteen 1</option>
+              <option value="canteen2">Canteen 2</option>
+              <option value="canteen3">Canteen 3</option>
             </select>
           </label>
         </div>
-        {feedbackType === "food" && (
-          <div>
-            <label>
-              Feedback Subtype:
-              <select
-                value={feedbackSubtype}
-                onChange={handleFeedbackSubtypeChange}
-              >
-                <option value="">Select a subtype</option>
-                <option value="canteen1">Canteen 1</option>
-                <option value="canteen2">Canteen 2</option>
-                <option value="canteen3">Canteen 3</option>
-              </select>
-            </label>
-          </div>
-        )}
-        {feedbackType === "app" && (
-          <div>
-            <label>
-              Feedback Subtype:
-              <select
-                value={feedbackSubtype}
-                onChange={handleFeedbackSubtypeChange}
-              >
-                <option value="">Select a subtype</option>
-                <option value="bugs">Bugs</option>
-                <option value="suggestions">Suggestions</option>
-              </select>
-            </label>
-          </div>
-        )}
+      )}
+      {feedbackType === "app" && (
         <div>
           <label>
-            Feedback:
-            <textarea
-              value={feedbackText}
-              onChange={handleFeedbackTextChange}
-            ></textarea>
+            Feedback Subtype:
+            <select
+              value={feedbackSubtype}
+              onChange={handleFeedbackSubtypeChange}
+            >
+              <option value="">Select a subtype</option>
+              <option value="bugs">Bugs</option>
+              <option value="suggestions">Suggestions</option>
+            </select>
           </label>
         </div>
-        <div>
-          {errorMessage && <p className="error-message">{errorMessage}</p>}
-          <button onClick={handleSubmit}>Submit</button>
-        </div>
+      )}
+      <div>
+        <label>
+          Feedback:
+          <textarea
+            value={feedbackText}
+            onChange={handleFeedbackTextChange}
+          ></textarea>
+        </label>
+      </div>
+      <div>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
       </div>
     </div>
   );
